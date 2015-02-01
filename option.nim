78ddb109c5b0b4e2ca77fbc2d5c40d1b90ef844d
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-01 10:00:09 +0900

    improve option
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- option.nim
+++ option.nim
@@ -3,34 +3,40 @@
 
 type
   OptionKind = enum
-    some
-    none
+    kSome
+    kNone
 
-  Option*[T] = object
+  TOption*[T] = object
     case kind: OptionKind
-    of some: v: T
-    of none: nil
+    of kSome: v: T
+    of kNone: nil
 
-proc `$`*[T](o: Option[T]): string =
+proc `$`*[T](o: TOption[T]): string =
   case o.kind
-  of some:
+  of kSome:
     "Some " & $o.v
-  of none:
+  of kNone:
     "None"
 
-proc isSome*[T](o: Option[T]): bool =
-  o.kind == some
+proc Some*[T](v: T): TOption[T] =
+  TOption[T](kind: kSome, v: v)
 
-proc isNone*[T](o: Option[T]): bool =
-  o.kind == none
+proc None*[T](): TOption[T] =
+  TOption[T](kind: kNone)
 
-proc unwrap*[T](o: Option[T]): T =
+proc isSome*[T](o: TOption[T]): bool =
+  o.kind == kSome
+
+proc isNone*[T](o: TOption[T]): bool =
+  o.kind == kNone
+
+proc unwrap*[T](o: TOption[T]): T =
   o.v
 
 when isMainModule:
-  let v1 = Option[int](kind: some, v: 1)
+  let v1 = Some(1)
   echo v1
   echo v1.unwrap
-  let v2 = Option[int](kind: none)
+  let v2 = None[int]()
   echo v2
   # echo v2.unwrap
