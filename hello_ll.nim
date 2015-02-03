2cebfbcff6726cb4f610cb59e939657ec7a1ce7b
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-03 15:51:15 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- hello_ll.nim
+++ hello_ll.nim
@@ -3,6 +3,37 @@ import session
 import reply
 import posix
 import logging
+import times
+
+let
+  TTL = Ttimespec(tv_sec:1.Time, tv_nsec:0)
+  CREATE_TIME = Ttimespec(tv_sec: 1381237736.Time, tv_nsec: 0)
+  DIR_ATTR = FileAttr (
+    ino: 1,
+    size: 0,
+    blocks: 0,
+    atime: CREATE_TIME,
+    mtime: CREATE_TIME,
+    ctime: CREATE_TIME,
+    mode: S_IFDIR and S_IRUSR,
+    nlink: 2,
+    uid: 501,
+    gid: 20,
+    rdev: 0,
+  )
+  TXT_ATTR = FileAttr (
+    ino: 2,
+    size: 13,
+    blocks: 1,
+    atime: CREATE_TIME,
+    mtime: CREATE_TIME,
+    ctime: CREATE_TIME,
+    mode: S_IFREG and S_IRUSR,
+    nlink: 1,
+    uid: 501,
+    gid: 20,
+    rdev: 0
+  )
 
 type Hello = ref object of LowlevelFs
 method lookup*(self: Hello, req: Request, parent: uint64, name: string, reply: Lookup) =
@@ -11,7 +42,13 @@ method lookup*(self: Hello, req: Request, parent: uint64, name: string, reply: L
 
 method getattr*(self: Hello, req: Request, ino: uint64, reply: GetAttr) =
   debug("Hello")
-  reply.err(-ENOSYS)
+  case ino.int
+  of 1:
+    reply.attr(TTL, DIR_ATTR)
+  of 2:
+    reply.attr(TTL, TXT_ATTR)
+  else:
+    reply.err(-ENOENT)
 
 method read*(self: Hello, req: Request, ino: uint64, fh: uint64, offset: uint64, size: uint32, reply: Read) =
   debug("Hello")
