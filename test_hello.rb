58914ac39c4c2ff23da5a5e28992ae917433fe4f
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-09 13:18:58 +0900

    add test_hello.rb
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

---
+++ test_hello.rb
@@ -0,0 +1,29 @@
+def s(cmd)
+  `sudo #{cmd}`
+end
+
+def mount(cmd, &block)
+  s "mkdir mnt"
+  pid = fork do
+    s cmd
+  end
+  block.call
+  s "kill -9 #{pid}"
+end
+
+def t(cmd, &block)
+  result = s cmd
+  exit false unless `$?` == 0
+  return unless block
+  exit false unless block.call(result)
+end
+
+mount "./hello mnt" do
+  t "ls mnt" do |result|
+    result.len == 1
+  end
+  t "ls -a mnt" do |result|
+    result.len == 3
+  end
+  t "cd mnt"
+end
