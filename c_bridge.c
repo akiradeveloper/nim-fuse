650ee597caa32b167ea8d06c49c2a8d97728dad2
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-11 08:35:56 +0900

    impl little bit
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- c_bridge.c
+++ c_bridge.c
@@ -1,7 +1,16 @@
 #include "fuse.h"
 
+void nim_bridge_destroy(int id, void *data);
+int nim_bridge_getattr(int id, const char *name, struct stat *st);
+
+int c_bridge_getattr(const char *name, struct stat *st)
+{
+	return nim_bridge_getattr(0, name, st);
+}
+
 void c_bridge_destroy(void *data)
 {
+	nim_bridge_destroy(0, data);
 }
 
 static struct fuse_operations c_bridge_ops = {
--- hifuse.nim
+++ hifuse.nim
@@ -1,13 +1,18 @@
 # Will be merged to fuse.nim
 # Separate files for development
 
+import posix
+
 {.passC: "-DFUSE_USE_VERSION=26".}
 {.passC: gorge("pkg-config --cflags fuse").}
 {.passL: gorge("pkg-config --libs fuse").}
 
-{. compile: "c_bridge.c" .}
+{.compile: "c_bridge.c".}
 
-proc nim_bridge_destroy(id: int, data: ptr) =
+proc nim_bridge_destroy(id: cint, data: pointer) {.exportc.} =
+  discard
+
+proc nim_bridge_getattr(id: cint, name: cstring, st: ptr TStat): cint {.exportc.} =
   discard
 
 type HiFuseFs = ref object of RootObj
