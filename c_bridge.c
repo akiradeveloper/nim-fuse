512c7a1d82d7eebdb3c2a2d564645bbcff40fa60
Akira Hayakawa <ruby.wktk@gmail.com>
2015-02-11 09:16:21 +0900

    add TFuseFileInfo
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- c_bridge.c
+++ c_bridge.c
@@ -1,7 +1,13 @@
 #include "fuse.h"
 
+int nim_bridge_releasedir(int id, const char *name, struct fuse_file_info *fi);
 void nim_bridge_destroy(int id, void *data);
 int nim_bridge_getattr(int id, const char *name, struct stat *st);
+
+int c_bridge_releasedir(const char *name, struct fuse_file_info *fi)
+{
+	return nim_bridge_releasedir(0, name, fi);
+}
 
 int c_bridge_getattr(const char *name, struct stat *st)
 {
@@ -15,6 +21,8 @@ void c_bridge_destroy(void *data)
 
 static struct fuse_operations c_bridge_ops = {
 	.destroy = c_bridge_destroy,
+	.getattr = c_bridge_getattr,
+	.releasedir = c_bridge_releasedir,
 };
 
 struct c_bridge_user_data {
--- hifuse.nim
+++ hifuse.nim
@@ -9,6 +9,20 @@ import posix
 
 {.compile: "c_bridge.c".}
 
+type TFuseFileInfo {.importc:"struct fuse_file_info", header:"<fuse.h>".} = object
+  flags: cint
+  fh_old: culong
+  writepage: cint
+  direct_io: cuint
+  keep_cache: cuint
+  flush: cuint
+  padding: cuint
+  fh: uint64
+  lock_owner: uint64
+
+proc nim_bridge_releasedir(id: cint, name: cstring, fi: ptr TFuseFileInfo): cint {.exportc.} =
+  discard
+
 proc nim_bridge_destroy(id: cint, data: pointer) {.exportc.} =
   discard
 
